bd95ea6f77dd725ccb3dd727da7a1fcd8c1efd67
Dane Merriman <merriman.dane02@gmail.com>
2022-05-01 20:38:31 -0400

    Created main function with driver

--- Source.cpp
+++ Source.cpp
@@ -18,3 +18,17 @@ int half(int num)
 	int num2 = round(static_cast<float>(num / 2));
 	return num2;
 }
+
+int main()
+{
+	double a = 7.0;
+	float b = 5.0f;
+	int c = 3;
+
+	cout << half(a) << endl;
+	cout << half(b) << endl;
+	cout << half(c) << endl;
+
+	system("pause");
+	return 0;
+}
